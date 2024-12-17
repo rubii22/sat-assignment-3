5c9ded62767e154a13db3c5af0576e1cd73390a2
RUBAB FATIMA <163537579+rubii22@users.noreply.github.com>
2024-12-17 18:41:07 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  
+
+  <div className="grid grid-rows-[100px_200px_200px_100px] grid-cols-3 gap-5">
+    <div className="bg-purple-400 col-span-3 text-[36px] font-bold text-center content-center border-2 border-white">Header</div>
+    <div className="bg-green-400 row-span-2 text-[36px] font-bold text-center content-center border-2 border-white">Sidebar</div>
+    <div className="bg-blue-400 col-span-2 text-[36px] font-bold text-center content-center border-2 border-white">Content-1</div>
+    <div className="bg-yellow-400 text-[36px] font-bold text-center content-center border-2 border-white">Content-2</div>
+    <div className="bg-orange-400 text-[36px] font-bold text-center content-center border-2 border-white">Content-3</div>
+    <div className="bg-pink-400 col-span-3 text-[36px] font-bold text-center content-center border-2 border-white">Footer</div>
+  </div>
+
+
+
+  );
+};
